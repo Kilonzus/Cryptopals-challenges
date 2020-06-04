2d028d538a822d13a68ff420224793bd44de014c
Kilonzus <officialkilonzo@gmail.com>
2020-06-04 14:48:27 -0700

    First commit

--- Set1/1-1.c
+++ Set1/1-1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char* binRep(int val) {
+	while (val!= 0) {
+		
+	}
+}
+
+char* hexToBin(char* arr) {
+	const char alpha[] = "0123456789ABCDEF"
+
+	return res
+}
+
 int main() {
 	const char base64[64]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','+','/'};
 
